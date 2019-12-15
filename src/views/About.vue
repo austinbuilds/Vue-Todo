5a9396c912ff46c07b1d3284a66a4ec41a258e82
AustinBuilds <54551778+austinbuilds@users.noreply.github.com>
2019-12-15 01:56:13 -0500

    Init commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <p>
+      This is the TodoList app v1.0.0. It is a part of the Vue crash course by
+      Brad Traversy on YouTube.
+    </p>
+  </div>
+</template>
